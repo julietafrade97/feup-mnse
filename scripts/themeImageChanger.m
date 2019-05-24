4425c07b0d0568c6f1777eab95ca8c113d3b545f
VenturaPereira <venturasousapereira@hotmail.com>
2019-05-24 10:20:15 +0100

    Add files via upload

---
+++ scripts/themeImageChanger.m
@@ -0,0 +1,27 @@
+
+s=51;
+year=1968;
+for i=1:s
+    
+    imagePath= ['assets/images/' num2str(year) '.jpg'];
+    
+
+    imOriginal = imread(imagePath);
+
+
+    im = (rgb2gray(imOriginal));
+
+
+    layerOne =(cat(3,im.*.886,im.*.478,im.*.247));
+    layer = layerOne;
+
+
+    
+    
+    baseFileName = [num2str(year) '.jpg'];
+    fullFileName = fullfile('assets/themeImages', baseFileName);
+    imwrite(layer, fullFileName);
+    year=year+1;
+    
+
+end
